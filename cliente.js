13736fec426e2caadd8032ed4cef43bd16d8f119
Glitch (maquetadocssgrid2) <none>
2018-07-10 15:07:44 +0000

    button

---
+++ cliente.js
@@ -0,0 +1,19 @@
+
+class Notas{
+
+  static subir(){
+
+    console.log('Hola,Martes con frio en BsAs');
+
+  document.querySelector("#panelMsg").innerHTML
+    += "Mensaje desde el Código,DATO";
+    
+    
+    
+    //    alert("Huy,acá de detiene el Programa Cliente y puedo Verificar Variables y Valores");  
+            
+
+    console.log('[OK] Hola,Martes con frio en BsAs');
+
+  }
+}
